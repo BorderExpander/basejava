45720b308363fee051bfb6f0327e374ae785b284
BorderExpander <ruax90@gmail.com>
2023-07-10 11:29:02 +0300

    HW 11. 1

---
+++ src/com/urise/webapp/util/deadlock.java
@@ -0,0 +1,40 @@
+package com.urise.webapp.util;
+
+class Table{
+}
+public class deadlock {
+    static final Table table1 = new Table();
+    static final Table table2 = new Table();
+    static public void algoritm1() {
+        synchronized (table1) {
+            printLog(1, 1);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            synchronized (table2) {
+                printLog(1, 2);
+            }
+        }
+    }
+
+    static public void algoritm2() {
+        synchronized (table2) {
+            printLog(2, 2);
+            synchronized (table1) {
+                printLog(2, 1);
+            }
+        }
+    }
+
+    static public void printLog(int algoritm, int table) {
+        System.out.printf("Алгоритм %d | Работа с таблицей %d%n", algoritm, table);
+    }
+
+    public static void main(String[] args) {
+        new Thread(deadlock::algoritm1).start();
+        new Thread(deadlock::algoritm2).start();
+    }
+}
